7fa6dc94571c2ab80c8b272f37f8e66d839bd92d
afropapp13 <afropapp@yahoo.gr>
2021-11-02 14:48:49 -0500

    v52 Nov 1 2021: 1) minor tweak

--- mySimFunctions.cpp
+++ mySimFunctions.cpp
@@ -10,6 +10,136 @@
 
 using namespace std;
 
+// -----------------------------------------------------------------------------------------------
+
+void CalcChiSquared(TH1D* h_model, TH1D* h_data, TH2D* cov, double &chi, int &ndof, double &pval) {
+
+	// Clone them so we can scale them 
+
+	TH1D* h_model_clone = (TH1D*)h_model->Clone();
+	TH1D* h_data_clone  = (TH1D*)h_data->Clone();
+	TH2D* h_cov_clone   = (TH2D*)cov->Clone();
+
+	// Getting covariance matrix in TMatrix form
+
+	TMatrix cov_m;
+	cov_m.Clear();
+	cov_m.ResizeTo(h_cov_clone->GetNbinsX(), h_cov_clone->GetNbinsX());
+
+	// loop over rows
+
+	for (int i = 0; i < h_cov_clone->GetNbinsX(); i++) {
+
+		// loop over columns
+
+		for (int j = 0; j < h_cov_clone->GetNbinsY(); j++) {
+
+			cov_m[i][j] = h_cov_clone->GetBinContent(i+1, j+1);
+		
+		}
+	
+	}
+
+	// Inverting the covariance matrix
+
+	cov_m.SetTol(1.e-23);
+	TMatrix inverse_cov_m = cov_m.Invert();
+	inverse_cov_m.SetTol(1.e-23);
+
+	// Calculating the chi2 = Summation_ij{ (x_i - mu_j)*E_ij^(-1)*(x_j - mu_j)  }
+	// x = data, mu = model, E^(-1) = inverted covariance matrix 
+
+	chi = 0.;
+	
+	for (int i = 0; i < h_cov_clone->GetNbinsX(); i++) {
+
+		for (int j = 0; j < h_cov_clone->GetNbinsY(); j++) {
+
+			double diffi = h_data_clone->GetBinContent(i+1) - h_model_clone->GetBinContent(i+1);
+			double diffj = h_data_clone->GetBinContent(j+1) - h_model_clone->GetBinContent(j+1);
+
+			double LocalChi = diffi * inverse_cov_m[i][j] * diffj; 
+			chi += LocalChi;
+//if (i == j) { cout << "i = " << i << " j = " << j << " diffi = " << diffi << " diffj = " << diffj << " chi = " << chi << " LocalChi = " << LocalChi << endl; }
+		}
+
+	}
+
+//cout << endl;
+
+	ndof = h_data_clone->GetNbinsX();
+	pval = TMath::Prob(chi, ndof);
+
+	//std::cout << "Chi2/dof: " << chi << "/" << h_data_clone->GetNbinsX() << " = " << chi/double(ndof) <<  std::endl;
+	//std::cout << "p-value: " <<  pval << "\n" <<  std::endl;
+
+	delete h_model_clone;
+	delete h_data_clone;
+	delete h_cov_clone;
+
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+
+TH1D* Multiply(TH1D* True, TH2D* SmearMatrix) {
+
+	TH1D* TrueClone = (TH1D*)(True->Clone());
+
+	int XBins = SmearMatrix->GetXaxis()->GetNbins();
+	int YBins = SmearMatrix->GetYaxis()->GetNbins();
+
+	if (XBins != YBins) { std::cout << "Not symmetric matrix" << std::endl; }
+
+	TVectorD signal(XBins);
+	TMatrixD response(XBins,XBins);
+
+	H2V(TrueClone, signal);
+	H2M(SmearMatrix, response, kTRUE);
+
+	TVectorD RecoSpace = response * signal;
+	V2H(RecoSpace, TrueClone);	
+
+	return TrueClone;
+
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+double Chi2Func(TH1D* h1,TH1D* h2, int LowBin = -1, int HighBin = -1) {
+
+	int NBinsX = h1->GetXaxis()->GetNbins();
+
+	double chi2 = 0;
+	
+	if (LowBin == -1) { LowBin = 0; }
+	if (HighBin == -1) { HighBin = NBinsX; }	
+
+	for (int WhichXBin = LowBin; WhichXBin < HighBin; WhichXBin++) {
+
+		double h1Entry = h1->GetBinContent(WhichXBin+1);
+		double h1Error = h1->GetBinError(WhichXBin+1);
+		double h2Entry = h2->GetBinContent(WhichXBin+1);
+		double h2Error = h2->GetBinError(WhichXBin+1);
+
+		double num = TMath::Power(h1Entry - h2Entry,2.);
+		double den = TMath::Power(h1Error,2.) + TMath::Power(h2Error,2.);
+		if (den != 0) { chi2 += (num / den); }
+
+	}
+
+	return chi2;
+
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+TString to_string_with_precision(double a_value, const int n = 3)
+{
+    std::ostringstream out;
+    out.precision(n);
+    out << std::fixed << a_value;
+    return TString(out.str());
+}
 // -------------------------------------------------------------------------------------------------------------------------------------
 
 double IntegratedXSec(TH1D* h) {
